48244ae1a638640a8c4d3b76b932851e40c91692
GitHub Action <action@github.com>
2024-02-17 15:48:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/anioutkazharkova/mobiusplugin/MyBundle.kt
+++ src/main/kotlin/com/github/anioutkazharkova/mobiusplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anioutkazharkova.mobiusplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/anioutkazharkova/mobiusplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/anioutkazharkova/mobiusplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.anioutkazharkova.mobiusplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.anioutkazharkova.mobiusplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
